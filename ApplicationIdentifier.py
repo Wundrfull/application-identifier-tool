cedaeaef81293443c898fe2f95ffe5b00a1d9624
Noah Peloquin <wundrfull.us@gmail.com>
2023-11-09 18:27:50 -0500

    Added stable script as main script

--- ApplicationIdentifier.py
+++ ApplicationIdentifier.py
@@ -10,7 +10,6 @@ class Application(tk.Tk):
         self.geometry("300x100")
         self.overlays = {}
         self.selected_window_title = tk.StringVar()
-        self.update_task_id = None
 
         # Dropdown for selecting the window to track
         self.dropdown = ttk.Combobox(self, textvariable=self.selected_window_title, width=50)
@@ -36,53 +35,41 @@ class Application(tk.Tk):
 
     def on_window_selection_change(self, event=None):
         """Handle the event when a new window is selected from the dropdown."""
-        # Cancel any existing update task
-        if self.update_task_id is not None:
-            self.after_cancel(self.update_task_id)
-            self.update_task_id = None
-
-        # Destroy all previous overlays
-        for overlay in self.overlays.values():
-            overlay[0].destroy()
-        self.overlays.clear()
-
         title = self.selected_window_title.get()
         if title:
             self.create_or_update_overlay(title)
 
     def create_or_update_overlay(self, title):
         target_window = gw.getWindowsWithTitle(title)[0]
-        # Create a new overlay
-        overlay = tk.Toplevel(self)
-        overlay.overrideredirect(True)
-        overlay.geometry(f"{target_window.width}x{target_window.height}+{target_window.left}+{target_window.top}")
-        overlay.lift()
-        overlay.wm_attributes("-topmost", True)
-        overlay.wm_attributes("-transparentcolor", "white")
+        if title in self.overlays:
+            # Update existing overlay
+            overlay, canvas = self.overlays[title]
+            overlay.geometry(f"{target_window.width}x{target_window.height}+{target_window.left}+{target_window.top}")
+            canvas.delete("all")  # Delete the old border and text
+        else:
+            # Create a new overlay
+            overlay = tk.Toplevel(self)
+            overlay.overrideredirect(True)
+            overlay.geometry(f"{target_window.width}x{target_window.height}+{target_window.left}+{target_window.top}")
+            overlay.lift()
+            overlay.wm_attributes("-topmost", True)
+            overlay.wm_attributes("-transparentcolor", "white")
 
-        canvas = tk.Canvas(overlay, highlightthickness=0, bg='white')
-        canvas.pack(fill=tk.BOTH, expand=True)
-        self.overlays[title] = (overlay, canvas)
-    
-        # Draw the border and text
-        self.update_overlay_canvas(canvas, target_window)
-
-        # Schedule updates
-        self.schedule_overlay_update(title, canvas)
-
-    def update_overlay_canvas(self, canvas, target_window):
-        canvas.delete("all")  # Clear previous drawings
+            canvas = tk.Canvas(overlay, highlightthickness=0, bg='white')
+            canvas.pack(fill=tk.BOTH, expand=True)
+            self.overlays[title] = (overlay, canvas)
+        
         # Draw the border and text
         canvas.create_rectangle(
             10, 10, target_window.width - 10, target_window.height - 10,
             outline='yellow', width=4, tag="border"
         )
         window_info = f"Width: {target_window.width}, Height: {target_window.height}, X: {target_window.left}, Y: {target_window.top}"
-        text_position = (target_window.width - 10, target_window.height - 20)
+        text_position = (target_window.width - 10, target_window.height - 10)
         # Black rectangle behind the text
         canvas.create_rectangle(
             text_position[0] - 220, text_position[1] - 20,
-            text_position[0], text_position[1] + 20,
+            text_position[0], text_position[1],
             fill="black", outline="black", tag="info_bg"
         )
         # Text in white for better contrast
@@ -91,17 +78,29 @@ class Application(tk.Tk):
             fill="white", font=("Helvetica", 9), tag="info"
         )
 
-    def schedule_overlay_update(self, title, canvas):
-        # Check if the window and canvas still exist before updating
-        if title in gw.getAllTitles():
-            self.update_overlay_canvas(canvas, gw.getWindowsWithTitle(title)[0])
-            # Reschedule the update
-            self.update_task_id = self.after(1000, lambda: self.schedule_overlay_update(title, canvas))
-        else:
-            # Window is closed, clean up
-            overlay = self.overlays.pop(title, None)
+        # Schedule updates
+        self.after(1000, lambda: self.update_overlay(title))
+
+    def update_overlay(self, title):
+        # If the window is closed, remove its overlay
+        if title not in gw.getAllTitles():
+            overlay, _ = self.overlays.pop(title, (None, None))
             if overlay:
-                overlay[0].destroy()
+                overlay.destroy()
+            return
+
+        # If the window exists, update the overlay
+        target_window = gw.getWindowsWithTitle(title)[0]
+        overlay, canvas = self.overlays.get(title, (None, None))
+        if overlay and canvas:
+            overlay.geometry(f"{target_window.width}x{target_window.height}+{target_window.left}+{target_window.top}")
+            canvas.coords("border", 10, 10, target_window.width - 10, target_window.height - 10)
+            window_info = f"Width: {target_window.width}, Height: {target_window.height}, X: {target_window.left}, Y: {target_window.top}"
+            text_position = (target_window.width - 10, target_window.height - 10)
+            canvas.coords("info_bg", text_position[0] - 220, text_position[1] - 20, text_position[0], text_position[1])
+            canvas.itemconfig("info", text=window_info)
+            canvas.coords("info", text_position)
+            self.after(1000, lambda: self.update_overlay(title))
 
     def run(self):
         self.mainloop()
